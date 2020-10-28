5ef8bd0072b41b09a497c4493d1873a43c08dd2f
mistergahan <54892798+mistergahan@users.noreply.github.com>
2020-10-29 02:04:43 +0300

    Bug fixes
    Bug in parsing pages was corrected

--- Parser/1.py
+++ Parser/1.py
@@ -3,9 +3,8 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-import os
 
-
+#https://auto.ru/moskva/cars/gaz/all/
 #URL = 'https://auto.ru/moskva/cars/mercedes/all/'
 HEADERS = {'user-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Safari/605.1.15', 'Accept': '*/*'}
 FILE = 'cars.csv'
@@ -35,10 +34,9 @@ def get_conent(html):
             'link': item.find('a', class_='Link ListingItemTitle-module__link').get('href'),
             'title': item.find('a', class_='Link ListingItemTitle-module__link').get_text(strip=True),
             'year': item.find('div', class_='ListingItem-module__year').get_text(strip=True),
-
         })
     #print(len(car))
-        return car
+    return car
 
 
 def save_file(items, path):
